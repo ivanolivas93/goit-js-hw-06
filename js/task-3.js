01ef30c4b9161e702518803dc0c664e7c546ff64
Juan Olivas <juanolivas0693@gmail.com>
2020-08-11 21:28:07 +0300

    module 06 modified

--- js/task-3.js
+++ js/task-3.js
@@ -85,9 +85,8 @@ const users = [
   },
 ];
 
+
 const getUsersWithGender = (users, gender) =>
-  users
-    .filter((user) => user.gender === gender)
-    .map((user) => ({ name: user.name }));
+  users.filter((user) => user.gender === gender).map((user) => user.name);
 
 console.log(getUsersWithGender(users, "male")); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
